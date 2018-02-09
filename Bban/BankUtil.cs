24c52546df514aa1a36deb4f9e9f0c62accc56cc
mitroskii <mitro.onatsu@hotmail.com>
2018-02-09 09:28:17 +0200

    BankUtilities created

---
+++ Bban/BankUtil.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Bban
+{
+    class BankUtil
+    {
+        public static string CorrectNumber(string accountNumber)
+        {
+            accountNumber = accountNumber.Replace("-", "").Replace(" ", "");
+            return accountNumber;
+        }
+    }
+}
---
+++ Bban/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Bban
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(BankUtil.CorrectNumber("haaa- looo -o    -- - - hehe"));
+            Console.ReadKey();
+        }
+    }
+}
