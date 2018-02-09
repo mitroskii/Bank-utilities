9ebe62eb2af46a199b314d8105b7d54e51361bee
mitroskii <mitro.onatsu@hotmail.com>
2018-02-09 11:22:08 +0200

    Bban started

--- Bban/BankUtil.cs
+++ Bban/BankUtil.cs
@@ -9,7 +9,48 @@ namespace Bban
         public static string CorrectNumber(string accountNumber)
         {
             accountNumber = accountNumber.Replace("-", "").Replace(" ", "");
+
+            int positionOfZeros = 0;
+            if (accountNumber[0] == '1' || accountNumber[0] == '2' || accountNumber[0] == '6' || accountNumber[0] == '8')
+                positionOfZeros = 6;
+            else if (accountNumber[0] == '3')
+            {
+                if (accountNumber[1] == '0' || accountNumber[1] == '2' || accountNumber[1] == '5')
+                    return "Account incorrect format!";
+                positionOfZeros = 6;
+            }
+            else if (accountNumber[0] == '4' || accountNumber[0] == '5')
+                positionOfZeros = 7;
+            else
+            {
+                return "Account unknown format!";
+            }
+
+            for(int i = accountNumber.Length; i < 14; i++)
+            {
+                accountNumber = accountNumber.Insert(positionOfZeros, "0");
+            }
             return accountNumber;
+        }
+
+        public static bool isValidAccount(string machineFormatAccount)
+        {
+            int checkNumber = int.Parse(machineFormatAccount[machineFormatAccount.Length-1].ToString());
+            machineFormatAccount = machineFormatAccount.Remove(machineFormatAccount.Length -1,1);
+            int[] multiplex = new int[2] { 2, 1 };
+            int m = 0;
+            for(int i =0; i < machineFormatAccount.Length; i++)
+            {
+                int n = int.Parse(machineFormatAccount[i].ToString()) * multiplex[i%2];
+                if(n>=10)
+                    m += n / 10 + (n - 10);
+                else
+                {
+                    m += n;
+                }
+            }
+            int calculatedCheckNumber = ((m / 10 + 1) * 10)-m;
+            return checkNumber == calculatedCheckNumber;
         }
     }
 }
--- Bban/Program.cs
+++ Bban/Program.cs
@@ -6,8 +6,13 @@ namespace Bban
     {
         static void Main(string[] args)
         {
-            Console.WriteLine(BankUtil.CorrectNumber("haaa- looo -o    -- - - hehe"));
+            string machineFormat = BankUtil.CorrectNumber("227720-35988");
+            Console.WriteLine(BankUtil.isValidAccount(machineFormat));
             Console.ReadKey();
         }
     }
 }
+//Oman pankkinumeron tarkistus
+//FI23 5482 3720 0153 38
+//     2121 2121 2121 2
+//    1+0+4+1+6+2+6+7+4+0+0+1+1+0+3+6=50-42=8
