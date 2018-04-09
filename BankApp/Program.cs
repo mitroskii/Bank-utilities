eff010e59d2d8de9bb803ed18b2fa1f8d575732a
mitroskii <mitro.onatsu@hotmail.com>
2018-04-09 09:03:12 +0300

    BankApp done

--- BankApp/Program.cs
+++ BankApp/Program.cs
@@ -51,10 +51,3 @@ namespace BankApp
         }
     }
 }
-//Oman pankkinumeron tarkistus
-//FI23 5482 3720 0153 38
-//     2121 2121 2121 2
-//    1+0+4+1+6+2+6+7+4+0+0+1+1+0+3+6=50-42=8
-//string machineFormat = BankUtil.CorrectNumber("227720-35988");
-//Console.WriteLine(BankUtil.isValidAccount(machineFormat));
-  //          Console.ReadKey();
